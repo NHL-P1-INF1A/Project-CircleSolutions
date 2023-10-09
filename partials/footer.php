da3ffda1922e62636de539ee1c2c403d94852a2f
Dave <dave.van.den.berg@student.nhlstenden.com>
2023-10-09 20:45:41 +0200

    Added logo + social icons to img folder and used them in footer.php, fixed some spacing in footer.css, changed divider to <hr>

--- partials/footer.php
+++ partials/footer.php
@@ -1,11 +1,20 @@
 <div id=footer>
     <div id=row1>
         <div>
-            Social Media Icons
+            <!-- Logo & Social Links -->
+            <img src="img/circle_solutions_logo_03.svg" alt="logo" id="footer_logo">
+            <div id="socials">
+                <a href="#"><img src="img/facebook.svg" alt="facebook"></a>
+                <a href="#"><img src="img/instagram.svg" alt="instagram"></a>
+                <a href="#"><img src="img/twitter.svg" alt="twitter"></a>
+            </div>
         </div>
-        <div>
-            <p><b>Nieuwsbrief</b></p>
-            <p>Abonneer op onze nieuwsbrief om altijd de allernieuwste updates te krijgen</p>
+        <!-- Nieuwsbrief inschrijven -->
+        <div id="subscribe_container">
+            <div>
+                <p><b>Nieuwsbrief</b></p>
+                <p>Abonneer op onze nieuwsbrief om altijd de allernieuwste updates te krijgen</p>
+            </div>
             <form action="index.php" method="POST" name="nieuwsbrief"> 
                 <div id=subscribe>
                     <div id="subscribe_email1">
@@ -18,7 +27,9 @@
             </form>
         </div>
     </div>
+    <!-- Copyright & Divider -->
     <div id=row2>
+        <hr>
         <div id=copyright>&copy; 2023 circle solutions. All Rights Reserved.</div>
     </div>
 </div>

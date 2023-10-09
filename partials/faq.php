10bbced6324ee95a1b3dbad145f6d9d1d4547fd9
mart-velema <mart.velema@student.nhlstenden.com>
2023-10-09 11:13:20 +0200

    FAQ Grid en Flexboxen aangemaakt

--- partials/faq.php
+++ partials/faq.php
@@ -4,16 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="../css/style_faq.css" rel="stylesheet">
+    <link href="../css/styles_faq.css" rel="stylesheet">
 </head>
 <body>
     <?php
         include "header.php"
     ?>
     <main>
-        <a href="../index.php">&lt;&lt;Home</a> <!-- Negeren, is voor Mart !-->
-        <div class="faq-grid">
-            <p>Hello, World!</p>
+        <div class="faq-outer-flex">
+            <h1>F.A.Q.</1>
+            <h2>Veel gestelde vragen</h2>
+            <a href="../index.php">&lt;&lt;Home</a> <!-- Negeren, is voor Mart !-->
+            <div class="faq-outer-grid">
+                <p>Hello, World!</p>
+            </div>
         </div>
     </main>
     <!--

aa26833ad23c05e98e60440fae8771824955074d
Yarno <minejarno@gmail.com>
2023-10-09 10:31:44 +0200

    Inserted all includes in index

--- index.php
+++ index.php
@@ -5,7 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cirle Solutions</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/png">
 </head>
 <body>
+    <?php
+
+    include "partials/navbar.php";
+
+    include "partials/header.php";
+
+    include "partials/about-us.php";
+
+    include "partials/values.php";
+
+    include "partials/services.php";
+
+    include "partials/faq.php";
+
+    include "partials/reviews.php";
+
+    include "partials/contact.php";
+
+    include "partials/footer.php";
+    ?>
 </body>
 </html>

8be06fb4d4b95d272d2e2f07a407bc0474835402
Luca Brockman <luca.brockman@student.nhlstenden.com>
2023-10-09 11:12:29 +0200

    added logo's, made setup for navbar and edited style for navbar

---
+++ partials/navbar.php
@@ -0,0 +1,13 @@
+<nav>
+    <a href="../index.php"><img src="img/circle_solutions_logo_darkblue.svg" alt="logocirclesolutions"></a>
+    <ul>
+        <li><a href="">Over Ons</a></li>
+        <li><a href="">Diensten</a></li>
+        <li><a href="">FAQ</a></li>
+        <li><a href="">Reviews</a></li>
+        <li><a href="">Contact</a></li>
+        <!-- <li><a href="">Nl</a></li> -->
+        <li><a href="">Darkmode</a></li>
+    </ul>
+
+</nav>

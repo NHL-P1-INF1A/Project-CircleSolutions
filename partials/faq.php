dd40a47b1a2f55c00136310121ff8ca13d6c5c6b
mart-velema <mart.velema@student.nhlstenden.com>
2023-10-09 19:34:31 +0200

    FAQ: shaduw toegevoegd aan de faq boxen

--- partials/faq.php
+++ partials/faq.php
@@ -10,25 +10,22 @@
     <?php
         include "header.php"
     ?>
-    <main>
-        <div id="faq-outer-flex">
-            <h1>F.A.Q.</1>
-            <h2>Veel gestelde vragen</h2>
-            <a id="faq-leesmeer" href="../index.php">Meer weergeven &gt;</a>
-            <div id="faq-inner-flex">
-                <div class="faq-popup-box-flex">
-                    <p>Hello, World!</p>
-                    <p>&darr;</p>
-                </div>
-                <div class="faq-popup-box-flex">
-                
-                </div>
-                <div class="faq-popup-box-flex">
-                
-                </div>
-                <div class="faq-popup-box-flex">
-                
-                </div>
+    <main  id="faq-outer-flex">
+        <h1>F.A.Q.</1>
+        <h2>Veel gestelde vragen</h2>
+        <a id="faq-leesmeer" href="../index.php">Meer weergeven &gt;</a>
+        <div id="faq-inner-flex">
+            <div class="faq-popup-box-flex">
+                <p>Hello, World!</p>
+                <button>&darr;</button>
+            </div>
+            <div class="faq-popup-box-flex">
+                <p>Hello, World!</p>
+                <p>&darr;</p>
+            </div>
+            <div class="faq-popup-box-flex">
+                <p>Hello, World!</p>
+                <p>&darr;</p>
             </div>
         </div>
     </main>

096aaf6c0ab159db06c25d8130dea39c4017edf5
mart-velema <mart.velema@student.nhlstenden.com>
2023-10-10 10:45:53 +0200

    FAQ: added comments to the PHP functions inside of faq.php and faq-supartials.php

--- partials/faq-subpartials.php
+++ partials/faq-subpartials.php
@@ -1,6 +1,8 @@
 <?php
+    #$faqtext is the array where the text of the faq popup box is stored in an array
+    #$location is the location of the text inside of the $faqtext array
     echo "<div class='faq-popup-box-flex'>
-            <p>$faqtext[$location]</p>
+            <p>$faqtext[$location]</p>      
             <button type='button' class='faq-popup-box-collapsible'><img src='../img/arrow-dropdown.svg' alt='&darr;'></button>
         </div>";
 ?>

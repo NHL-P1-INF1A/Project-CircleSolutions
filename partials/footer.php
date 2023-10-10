b93ec1de1dd3b83449373ac746f39220fc610ab4
Dave <dave.van.den.berg@student.nhlstenden.com>
2023-10-10 10:36:59 +0200

    Added comments, moved css code to be chronological, made input field bigger, added additional wrapping and gaps

--- partials/footer.php
+++ partials/footer.php
@@ -1,35 +1,33 @@
 <div id=footer>
-    <div id=row1>
+    <div id=footer_container>
         <div>
             <!-- Logo & Social Links -->
             <img src="img/circle_solutions_logo_03.svg" alt="logo" id="footer_logo">
-            <div id="socials">
+            <div id="footer_socials">
                 <a href="#"><img src="img/facebook.svg" alt="facebook"></a>
                 <a href="#"><img src="img/instagram.svg" alt="instagram"></a>
                 <a href="#"><img src="img/twitter.svg" alt="twitter"></a>
             </div>
         </div>
         <!-- Nieuwsbrief inschrijven -->
-        <div id="subscribe_container">
+        <div id="footer_subscribe_container">
             <div>
                 <p><b>Nieuwsbrief</b></p>
                 <p>Abonneer op onze nieuwsbrief om altijd de allernieuwste updates te krijgen</p>
             </div>
             <form action="index.php" method="POST" name="nieuwsbrief"> 
-                <div id=subscribe>
-                    <div id="subscribe_email1">
-                        <input type="email" name="email" id="subscribe_email" placeholder="example@gmail.com" required> 
+                <div id=footer_subscribe>
+                    <div>
+                        <input type="email" name="email" id="footer_subscribe_email" placeholder="example@gmail.com" required size="45"> 
                     </div>
                     <div>
-                        <input type="submit" value="Subscribe" id="subscribe_submit">
+                        <input type="submit" value="Subscribe" id="footer_subscribe_submit">
                     </div>
                 </div>
             </form>
         </div>
     </div>
     <!-- Copyright & Divider -->
-    <div id=row2>
-        <hr>
-        <div id=copyright>&copy; 2023 circle solutions. All Rights Reserved.</div>
-    </div>
+    <hr>
+    <div id=footer_copyright>&copy; 2023 circle solutions. All Rights Reserved.</div>
 </div>

ef3d749d2658ae003de6949a49f8f5c8064ef3a4
mart-velema <mart.velema@student.nhlstenden.com>
2023-10-10 12:06:21 +0200

    FAQ-page: added all the text for the popup boxes

--- partials/faq-page.php
+++ partials/faq-page.php
@@ -10,14 +10,26 @@
     <?php
         include "../partials/header.php"
     ?>
-    <main  class="faq-outer-flex">
+    <main class="faq-outer-flex">
         <!--Header!-->
         <h1>F.A.Q.</h1>
         <h2>Veel gestelde vragen</h2>
+        <div class="faq-page-breakline"></div>
+        <!--Flexbox for popup boxes!-->
         <div class="faq-inner-flex">
             <!--PHP function to make the faq popup boxes!-->
             <?php
-                $faqtext = array("Wat is een intranet?","Wat is het verschil tussen internet en intranet?","Wat is een intranet en een extranet?");  #array with all the text that will be shown initially
+                $faqtext = array(
+                    "Wat is een intranet?",
+                    "Wat is het verschil tussen internet en intranet?",
+                    "Wat is een intranet en een extranet?",
+                    "Wat zijn intranetwebsites?",
+                    "Wat zijn intranet toepassingen?",
+                    "Wat is een intranet portaal?",
+                    "Kan een intranet werken zonder internet?",
+                    "Kan een intranet op afstand worden geopend?",
+                    "Wat doet een intranet?"
+                );  #array with all the text that will be shown initially
                 include "faq-subpartials.php"
             ?>
         </div>

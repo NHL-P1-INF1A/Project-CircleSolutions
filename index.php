35a9318d3a18658e1d25c6d5e41653b44c4e1403
Yarno <minejarno@gmail.com>
2023-10-10 17:13:11 +0200

    Changed all classes and ids from faq into snakecase | Made faq functional on multiple buttons | faq answers are now in the array aswell

--- index.php
+++ index.php
@@ -42,13 +42,23 @@
 </body>
 <script src="js/carousel.js"></script>
 <script>
-document.getElementById('toggle-button').addEventListener('click', function() {
-    var textElement = document.getElementById('faq-text');
-    if (textElement.style.display === 'none' || textElement.style.display === '') {
-        textElement.style.display = 'block';
-    } else {
-        textElement.style.display = 'none';
-    }
+// Get all elements with the 'toggle-button' class
+var buttons = document.querySelectorAll('.toggle_button');
+
+// Loop through each button and add a click event listener
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Get the target element ID from the data attribute
+        var targetId = this.getAttribute('data-target');
+        var textElement = document.getElementById(targetId);
+
+        // Toggle the display of the target element
+        if (textElement.style.display === 'none' || textElement.style.display === '') {
+            textElement.style.display = 'block';
+        } else {
+            textElement.style.display = 'none';
+        }
+    });
 });
 </script>
 </html>

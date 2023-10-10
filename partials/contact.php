550b1531a219faa7bdeb8a6547527ff6426426b1
Aaron de Bruin <aaron.de.bruin@student.nhlstenden.com>
2023-10-10 09:33:26 +0200

    made basic contact form without complete css

---
+++ partials/contact.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class=contact-form>
+    <div class="contact-left">
+    <h2>voor vragen, neem gerust contact op</h2>
+    <div class="contact-links">
+    <p>contact@circlesolutions.com </p>
+    <p> +31 06 123 123 123 </p>  
+    <p> Van Schaikweg 94, 7811 KL Emmen </p>
+    </div>
+    </div>
+    <div class="contact-right">
+    <p>hello</p>
+    </div>
+</div>
+</div>

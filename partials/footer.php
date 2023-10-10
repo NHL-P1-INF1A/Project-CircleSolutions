1d96fcc6633a3b64df9c8d5917f235976f6a4dff
Yarno <minejarno@gmail.com>
2023-10-10 10:50:01 +0200

    Added the footer.php | Added the footer.css | Added the svg files to img folder for the footer

---
+++ partials/footer.php
@@ -0,0 +1,33 @@
+<div id=footer>
+    <div id=footer_container>
+        <div>
+            <!-- Logo & Social Links -->
+            <img src="img/circle_solutions_logo_03.svg" alt="logo" id="footer_logo">
+            <div id="footer_socials">
+                <a href="#"><img src="img/facebook.svg" alt="facebook"></a>
+                <a href="#"><img src="img/instagram.svg" alt="instagram"></a>
+                <a href="#"><img src="img/twitter.svg" alt="twitter"></a>
+            </div>
+        </div>
+        <!-- Nieuwsbrief inschrijven -->
+        <div id="footer_subscribe_container">
+            <div>
+                <p><b>Nieuwsbrief</b></p>
+                <p>Abonneer op onze nieuwsbrief om altijd de allernieuwste updates te krijgen</p>
+            </div>
+            <form action="index.php" method="POST" name="nieuwsbrief"> 
+                <div id=footer_subscribe>
+                    <div>
+                        <input type="email" name="email" id="footer_subscribe_email" placeholder="example@gmail.com" required size="45"> 
+                    </div>
+                    <div>
+                        <input type="submit" value="Subscribe" id="footer_subscribe_submit">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- Copyright & Divider -->
+    <hr>
+    <div id=footer_copyright>&copy; 2023 circle solutions. All Rights Reserved.</div>
+</div>

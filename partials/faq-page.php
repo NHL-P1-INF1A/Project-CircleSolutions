4abcc89d613655b4854a48e2a4f67eb421466918
mart-velema <mart.velema@student.nhlstenden.com>
2023-10-10 11:54:28 +0200

    FAQ-page: added FAQ-page to serve all faq

---
+++ partials/faq-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Circle Soutions - FAQ</title>
+    <link href="styles_faq.php" rel="stylesheet">
+</head>
+<body>
+    <?php
+        include "../partials/header.php"
+    ?>
+    <main  id="faq-outer-flex">
+        <!--Header!-->
+        <h1>F.A.Q.</h1>
+        <h2>Veel gestelde vragen</h2>
+    </main>
+    <?php
+        include "../partials/footer.php"
+    ?>
+</body>
+</html>

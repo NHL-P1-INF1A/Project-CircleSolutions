f500d4ce849259e6bffed8fa1b2972a49946d24b
Lennon G Spaenij <Lennon.g.spaeny@gmail.com>
2023-10-11 11:28:27 +0200

    added images to the img-folder, made the first blocks for the deals section,first items to the block, added service.css file to the CSS-folder

---
+++ partials/services.php
@@ -0,0 +1,39 @@
+    <section id="services" class="container">
+        <div class="container_services_text">
+            <p> diensten </p>
+            <p> onze services </p>
+        </div>
+        <div class="container_services_deals">
+            <div class="container_services_workspace">
+                <div class="container_workspace_wrapper">
+                    <div class="img_service_circle">
+                        <img src="./img/deal_circle.png" name="deal_circle" alt="Icon deal circle" class="icon_circle">
+                        circle workspace
+                    </div>
+                    <div class="circle_text_1">
+                            Circle Workspace maakt het beheren van apps, communicatie, en documenten eenvoudig en efficiënt.
+                    </div>
+                    <div class="circle_text_2">
+                        voor 1-50 Gebruikers
+                        <p><h2 class="money_text">&euro;140</h2> /maandelijks </p>
+                        <p>voor 51-500 Gebruikers </P>
+                        <p><h2 class="money_text">&euro;740</h2> /maandelijks </p>
+                    </div>
+                    <div class="circle_text_3"> 
+                        inhoud
+                    </div>
+                    <div>
+                        <p> <img src="./img/check_circle.png" name="check_circle" alt="check_circle" class="check_circle"> app-beheer </P>
+                        <p> <img src="./img/check_circle.png" name="check_circle" alt="check_circle" class="check_circle"> communicatie en informatie </P>
+                        <p> <img src="./img/check_circle.png" name="check_circle" alt="check_circle" class="check_circle"> document beheer </P>
+                    </div>
+                    <div class="circle_button">
+                        meer informatie
+                    </div>
+                </div>
+            </div>
+            <div class="container_services_diamond">
+                <p>test</p>
+            </div>
+        </div>
+    </section>

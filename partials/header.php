e1fb1f13c6167f44a4162c2d2ae63d26cd8fc838
Luca Brockman <luca.brockman@student.nhlstenden.com>
2023-10-12 11:34:36 +0200

    headerpage added content and added style

--- partials/header.php
+++ partials/header.php
@@ -3,10 +3,17 @@
         include "partials/navbar.php";
     ?>
     <section id="banner" class="container"> 
-        <!-- id naam moet nog verandert worden -->
-    <p><h1>just connect the dots</h1>
-    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. ut laoreet nisi ligula, sit amet ullamcorper risus tristique ac. Suspendisse potenti.</h3>
-    </p>
+    <div id="banner_text">
+        <p>
+            <h1>
+                just connect the dots
+            </h1>
+            <img src="./img/line_small.svg" alt="">
+            <h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. ut laoreet nisi ligula, sit amet ullamcorper risus tristique ac. Suspendisse potenti.
+            </h3>
+        </p>
+    </div>
     <img src="img/hero.svg" alt="hero" id="headerimg">
     </section>
 </header>

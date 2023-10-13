647d7cde92bb4a62b61613994923b204a5756cf6
Aaron de Bruin <aaron.de.bruin@student.nhlstenden.com>
2023-10-13 09:21:16 +0200

    make click function when button is clicked it gets selected button class

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
 const contactLinks = document.querySelector('.contact-links'); 
-const contactRight = document.querySelector('.contact-links-right')
-const mediaQuery = window.matchMedia('(max-width: 1280px)')
+const contactRight = document.querySelector('.contact-links-right');
+const mediaQuery = window.matchMedia('(max-width: 1280px)');
 
 function handleMediaChange(event) {
   if (event.matches) {
@@ -15,6 +15,29 @@ function handleMediaChange(event) {
 }
 mediaQuery.addListener(handleMediaChange);
 handleMediaChange(mediaQuery);
+
+
+// Get all the buttons
+const buttons = document.querySelectorAll('.buttons-contact-right button');
+
+// Add a click event listener to each button
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Remove selected-button class from all buttons
+        buttons.forEach(btn => {
+            btn.classList.remove('selected-button');
+            btn.classList.add('unselected-button');
+        });
+
+        // Add selected-button class to the clicked button
+        button.classList.remove('unselected-button');
+        button.classList.add('selected-button');
+    });
+});
+
+
+
+
 
 
 

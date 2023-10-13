c1ff31d144a07607b3e9973fd730163ea352a471
Bram Suurd <bram.suurd@student.nhlstenden.com>
2023-10-13 12:00:57 +0200

    fixed a bug where arrows weren't showing in the FAQ section

--- api/faq-page.php
+++ api/faq-page.php
@@ -43,7 +43,7 @@
                         "Ja, een intranet kan worden geopend op verschillende apparaten, ongeacht de locatie - alles wat nodig is, is een internetverbinding. Dit is ideaal voor elke organisatie met verspreide of frontlinie-werknemers die communicatiemiddelen en toegang tot organisatie-informatie nodig hebben, waar ze zich ook bevinden.",
                         "Intranetsoftware biedt allerlei organisatorische oplossingen, kan de betrokkenheid van werknemers verbeteren, de communicatie ondersteunen en de samenwerking vergroten. Moderne intranetten zijn nu grotendeels gebaseerd in de cloud, wat wereldwijde organisaties in staat stelt om snel en gemakkelijk verbinding te maken, kennis te delen en nieuws uit te zenden."   
                     );  #array with all the text that will be shown once the popup box button has been clicked
-                    include "./partials/faq-subpartials.php";
+                    include "partials/faq-subpartials.php";
                 ?>
             </div>
         </main>

c6175d71fc2e01b3fd20db800cc2247011dfcb99
yarnobachmann <minejarno@gmail.com>
2023-10-16 09:41:37 +0200

    Missed a few links, these are now rerouted

--- circle_dots.php
+++ circle_dots.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Circle Solutions</title>
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <link rel="shortcut icon" href="./img/favicon.png" type="image/png">
 </head>
 
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Circle Solutions</title>
     <link rel="stylesheet" href="./css/styles.css">
-    <link rel="stylesheet" href="./css/reviews.css">
     <link rel="shortcut icon" href="./img/favicon.png" type="image/png">
 </head>
 <body>

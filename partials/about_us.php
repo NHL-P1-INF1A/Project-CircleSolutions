0b25d11922576c622f7bdc05973ccdf82d8cd10c
yarnobachmann <minejarno@gmail.com>
2023-10-20 10:35:27 +0200

    Nav linking changed to have a margin so it scrolls to right position | changed review to have an extra div hidden container to hide the not showing items | Fixed the container position in about_us

--- partials/about_us.php
+++ partials/about_us.php
@@ -1,16 +1,14 @@
-<div class="container">
-    <div id="about_us">
-        <div id="about_us_textbox">
-            <h2>wie zijn wij</h2>
-            <p><b>
-                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Ut enim ad minim veniam, quis
-            </b></p>
-            <p>
-                <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate
-            </p>
-        </div>
-        <img src="./img/wie-zijn-wij.svg" alt="Wie zijn wij">
+<div class="container" id="about_us">
+    <div id="about_us_textbox">
+        <h2>wie zijn wij</h2>
+        <p><b>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis
+        </b></p>
+        <p>
+            <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate
+        </p>
     </div>
+    <img src="./img/wie-zijn-wij.svg" alt="Wie zijn wij">
 </div>

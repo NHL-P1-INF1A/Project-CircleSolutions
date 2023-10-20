0b52aabab68d159a74d50a8fb2d9bb7354c2495d
Aaron de Bruin <aaron.de.bruin@student.nhlstenden.com>
2023-10-20 12:03:40 +0200

    change hamburger icon on scroll

--- partials/header.php
+++ partials/header.php
@@ -1,23 +1,24 @@
 <header>
     <?php
-        include "navbar.php";
+    include "navbar.php";
     ?>
-    <section id="banner" class="container"> 
+    <section id="banner" class="container">
         <div id="banner_items">
             <div class="banner_text">
                 <h2>just connect the dots</h2>
                 <?php
-                    include "line_small.php";
+                include "line_small.php";
                 ?>
-                <h3>Ons doel is om software te ontwikkelen die past bij de individuele kwaliteiten van individuele behoeften. Mensen en bedrijven sterker maken met softwareoplossingen.</h3>
+                <h3>Ons doel is om software te ontwikkelen die past bij de individuele kwaliteiten van individuele
+                    behoeften. Mensen en bedrijven sterker maken met softwareoplossingen.</h3>
                 <a class="circle_button" href="index.php#about-us">beginnen</a>
             </div>
             <div class="banner_image">
-                    <img src="./img/hero.svg" alt="hero" id="headerimg">
+                <img src="./img/hero.svg" alt="hero" id="headerimg">
             </div>
         </div>
         <div class="banner_arrow_down">
-                <a href="index.php#about-us" ><img src="./img/circle_arrow_down.svg" alt="arrow down"></a>
+            <a href="index.php#about_us"><img src="./img/circle_arrow_down.svg" alt="arrow down"></a>
         </div>
     </section>
 </header>

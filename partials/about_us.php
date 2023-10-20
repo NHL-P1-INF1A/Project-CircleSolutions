e504a2fdb4766344999f1bbeb508359dc6f57326
Lennon G Spaenij <Lennon.g.spaeny@gmail.com>
2023-10-20 10:46:12 +0200

    added a container class in about_us.php

--- partials/about_us.php
+++ partials/about_us.php
@@ -1,17 +1,15 @@
-<div class="container">
-    <div id="about_us">
-        <div id="about_us_textbox">
-            <h2>wie zijn wij</h2>
-            <p><b>
-                Met productiesoftware optimaliseert circle solutions bedrijfsprocessen.
-                Ze maken de cirkel rond; en besef dat deze cirkel voor ieder bedrijf uniek is.
-            </b></p>
-            <p>
-                <br>Zo intuïtief als een bezoek aan elkaars kantoor; zo werkt de software van circle solutions
-                    verbindt waar nodig. Zo wordt gebruiksvriendelijke software ontwikkeld.
-                    Geen overbodige functionaliteiten maar alleen verbindingen waar nodig; Just connect the dots.
-            </p>
-        </div>
-        <img src="./img/wie-zijn-wij.svg" alt="Wie zijn wij">
+<div class="container" id="about_us">
+    <div id="about_us_textbox">
+        <h2>wie zijn wij</h2>
+        <p><b>
+            Met productiesoftware optimaliseert circle solutions bedrijfsprocessen.
+            Ze maken de cirkel rond; en besef dat deze cirkel voor ieder bedrijf uniek is.
+        </b></p>
+        <p>
+            <br>Zo intuïtief als een bezoek aan elkaars kantoor; zo werkt de software van circle solutions
+                verbindt waar nodig. Zo wordt gebruiksvriendelijke software ontwikkeld.
+                Geen overbodige functionaliteiten maar alleen verbindingen waar nodig; Just connect the dots.
+        </p>
     </div>
+    <img src="./img/wie-zijn-wij.svg" alt="Wie zijn wij">
 </div>

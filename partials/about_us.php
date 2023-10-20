4ca23541f5ce3750278ef23186d35f10cb8192f4
Lennon G Spaenij <Lennon.g.spaeny@gmail.com>
2023-10-20 10:54:00 +0200

    trying to resolve a merge error in about_us.php

--- partials/about_us.php
+++ partials/about_us.php
@@ -2,13 +2,12 @@
     <div id="about_us_textbox">
         <h2>wie zijn wij</h2>
         <p><b>
-            Met productiesoftware optimaliseert circle solutions bedrijfsprocessen.
-            Ze maken de cirkel rond; en besef dat deze cirkel voor ieder bedrijf uniek is.
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis
         </b></p>
         <p>
-            <br>Zo intuïtief als een bezoek aan elkaars kantoor; zo werkt de software van circle solutions
-                verbindt waar nodig. Zo wordt gebruiksvriendelijke software ontwikkeld.
-                Geen overbodige functionaliteiten maar alleen verbindingen waar nodig; Just connect the dots.
+            <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate
         </p>
     </div>
     <img src="./img/wie-zijn-wij.svg" alt="Wie zijn wij">

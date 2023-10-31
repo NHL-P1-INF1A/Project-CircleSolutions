ad373f814f300cc91ef65c621ddbb52543dcff3e
yarnobachmann <minejarno@gmail.com>
2023-10-31 10:26:21 +0100

    fixed meta tag of viewport that gives error in validator

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Circle Solutions</title>
     <link rel="stylesheet" href="./css/styles.css">

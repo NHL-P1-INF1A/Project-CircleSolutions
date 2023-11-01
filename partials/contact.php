4560b2dd524484b7a56c0d7ab01607daeadc3712
Bram Suurd <bram.suurd@student.nhlstenden.com>
2023-11-01 10:09:36 +0100

    added a hover effect to boxes on workspace and dots sites.

--- partials/contact.php
+++ partials/contact.php
@@ -37,7 +37,7 @@
                         bericht versturen</button>
                     <div class="contact_links_right">
                         <div class="contact_mail">
-                            <p><i><img src="./img/e-mail.svg" alt="e-mail"></i><a href="mailto:contact@circlesolutions.com">contact@circlesolutions.com</a> </p>
+                        <a href="mailto:contact@circlesolutions.com"><i><img src="./img/e-mail.svg" alt="e-mail"></i>contact@circlesolutions.com</a>
                         </div>
                         <div class="contact_phone">
                             <p> <i> <img src="./img/phone.svg" alt="phone"></i>+31 06 123 123 123 </p>

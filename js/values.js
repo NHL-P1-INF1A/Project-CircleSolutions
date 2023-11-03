feaa1822eca8415d4767819f041f811c640f6b92
Bram Suurd <bram.suurd@student.nhlstenden.com>
2023-11-03 13:25:50 +0100

    made it so values.js follows the code conventions of NHLSTENDEN

--- js/values.js
+++ js/values.js
@@ -5,23 +5,27 @@ counters.forEach(counter => originalValues.push(counter.innerText));
 let animated = [];
 
 // Function to check if an element is in the viewport
-function isInViewport(element) {
+function isInViewport(element) 
+{
   let rect = element.getBoundingClientRect();
   return (rect.top <= window.innerHeight && rect.bottom >= 0);
 }
 
 // Easing function for a smooth counting effect
-function easeInOutCubic(t) {
+function easeInOutCubic(t) 
+{
   return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
 }
 
 // Function to animate the counter numbers
-function animateNumber(counterElement, targetValue) {
+function animateNumber(counterElement, targetValue) 
+{
   let startTime = null; // To keep track of the animation start time
   let duration = 2500;  // Duration of the animation in milliseconds (2 seconds)
 
   // Function that updates the counter based on eased progress
-  function updateCounter(timestamp) {
+  function updateCounter(timestamp) 
+  {
     if (!startTime) startTime = timestamp;
 
     let elapsed = timestamp - startTime;
@@ -32,9 +36,11 @@ function animateNumber(counterElement, targetValue) {
     counterElement.innerText = currentValue;
 
     // Continue the animation if not yet complete
-    if (progress < 1) {
+    if (progress < 1) 
+    {
       requestAnimationFrame(updateCounter);
-    } else {
+    } else 
+    {
       // Ensure the counter displays the final target value
       counterElement.innerText = targetValue;
     }
@@ -45,16 +51,21 @@ function animateNumber(counterElement, targetValue) {
 }
 
 // Listen for scroll events to trigger the animation
-window.addEventListener("scroll", function() {
-  counters.forEach((counter, index) => {
-    if (isInViewport(counter)) {
-      if (!animated[index]) {
+window.addEventListener("scroll", function() 
+{
+  counters.forEach((counter, index) => 
+  {
+    if (isInViewport(counter)) 
+    {
+      if (!animated[index]) 
+      {
         let targetValue = parseInt(originalValues[index], 10);
         counter.innerText = '0'; // Reset to initial value
         animateNumber(counter, targetValue);
         animated[index] = true; // Mark as animated
       }
-    } else {
+    } else 
+    {
       // Mark the counter as not animated when it goes out of view
       animated[index] = false;
     }
